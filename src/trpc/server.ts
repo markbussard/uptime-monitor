77e5e98adf237e62d5b8a374f9bdcaf1b5342b15
markbussard <markbussard@outlook.com>
2024-03-30 00:28:53 -0700

    Fix linting issue

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -10,7 +10,7 @@ import { createCaller } from "~/server/root";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
+const createContext = cache(() => {
   const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
