2dfac402c6c84dd45ecfa21ed2789105a3ac5e30
markbussard <markbussard@outlook.com>
2024-04-15 00:35:00 -0700

    Remove white text color on hover and gray text color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = (props: FooterProps) => {
   return (
     <footer
       className={cn(
-        "flex h-20 items-center justify-between bg-dark-800 px-16 text-gray-400",
+        "flex h-20 items-center justify-between px-16",
         props.className
       )}
     >
@@ -18,10 +18,10 @@ export const Footer = (props: FooterProps) => {
         &copy; 2023 Uptime Monitor. All rights reserved.
       </p>
       <section className="flex gap-8">
-        <Link className="hover:text-white hover:underline" href="/terms">
+        <Link className="hover:underline" href="/terms">
           Terms
         </Link>
-        <Link className="hover:text-white hover:underline" href="/privacy">
+        <Link className="hover:underline" href="/privacy">
           Privacy
         </Link>
       </section>
