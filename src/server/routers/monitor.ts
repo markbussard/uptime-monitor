528c70016b78d1711d6c95794346cfe271cbf8c2
markbussard <markbussard@outlook.com>
2024-04-28 01:22:14 -0700

    Monitor router updates Add initial functionality for list and create functions

--- src/server/routers/monitor.ts
+++ src/server/routers/monitor.ts
@@ -1,9 +1,27 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/trpc";
+import { CreateMonitorSchema } from "~/server/validators";
 
 export const monitorRouter = createTRPCRouter({
-  list: protectedProcedure.query(() => {
-    return {
-      message: "Hello, world!"
-    };
-  })
+  list: protectedProcedure.query(async ({ ctx }) => {
+    const userId = ctx.auth.userId;
+
+    const monitors = await ctx.db.monitor.findMany({
+      where: {
+        userId
+      }
+    });
+    return monitors;
+  }),
+  create: protectedProcedure
+    .input(CreateMonitorSchema)
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.auth.userId;
+      const newMonitor = await ctx.db.monitor.create({
+        data: {
+          userId,
+          ...input
+        }
+      });
+      return newMonitor;
+    })
 });
